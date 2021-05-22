97917c651c79f891057fe63da6ffb1d97632c75c
AliWindsor <aliwindsor@gmail.com>
2021-05-22 21:38:52 +0100

    Initial Commit

---
+++ LastFM Song Search/LastFM_Song_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastFM_Song_SearchApp.swift
+//  LastFM Song Search
+//
+//  Created by Alicia Windsor on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LastFM_Song_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
